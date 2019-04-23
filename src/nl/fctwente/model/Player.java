897fd33d501adcce03bac87ec6b9c6f88639242e
Raymond Loman <raymond@carpago.nl>
2019-04-23 11:19:24 +0200

    Make the first model class Player

---
+++ src/nl/fctwente/model/Player.java
@@ -0,0 +1,19 @@
+package nl.fctwente.model;
+
+public class Player {
+
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
